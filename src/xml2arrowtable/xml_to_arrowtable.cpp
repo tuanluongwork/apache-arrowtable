c464ffb1f6b5a908109fdcb8f7caad28748b156d
tuanluongwork <tuan.luong@opswat.com>
2022-05-10 22:13:30 +0700

    parse xml

--- src/xml2arrowtable/xml_to_arrowtable.cpp
+++ src/xml2arrowtable/xml_to_arrowtable.cpp
@@ -30,7 +30,7 @@ int XmlToArrowTable::execute()
 
 	XercesDOMParser* parser = new XercesDOMParser();
 	parser->setValidationScheme(XercesDOMParser::Val_Always);
-	parser->setDoNamespaces(true);    // optional
+	parser->setDoNamespaces(true);
 
 	ErrorHandler* errHandler = (ErrorHandler*) new HandlerBase();
 	parser->setErrorHandler(errHandler);
@@ -39,6 +39,93 @@ int XmlToArrowTable::execute()
 
 	try {
 		parser->parse(xmlFile);
+
+		//create unicode strings to for later use
+        XMLCh *book = XMLString::transcode("book");
+        XMLCh *category = XMLString::transcode("category");
+        XMLCh *title = XMLString::transcode("title");
+        XMLCh *author = XMLString::transcode("author");
+        XMLCh *editions = XMLString::transcode("editions");
+        XMLCh *edition = XMLString::transcode("edition");
+        XMLCh *isbn = XMLString::transcode("isbn");
+        XMLCh *year = XMLString::transcode("year");
+        XMLCh *price = XMLString::transcode("price");
+
+		DOMDocument *doc = parser->getDocument();
+		DOMNodeList *bookList = doc->getElementsByTagName(book);
+		XMLSize_t bookLen = bookList->getLength();
+		
+		for (XMLSize_t i = 0; i < bookLen; i++) {
+			DOMNode *bookNode = bookList->item(i);
+			cout << "category: " << XMLString::transcode(bookNode->getAttributes()->getNamedItem(category)->getTextContent()) << endl;
+			DOMElement *bookElm = dynamic_cast<DOMElement*>(bookNode);
+
+			cout << "\ttitle: " << XMLString::transcode(bookElm->getElementsByTagName(title)->item(0)->getTextContent()) << endl;
+			cout << "\tauthor: " << XMLString::transcode(bookElm->getElementsByTagName(author)->item(0)->getTextContent()) << endl;
+			DOMNodeList *editionList = bookElm->getElementsByTagName(edition);
+			XMLSize_t editionLen = editionList->getLength();
+			for (XMLSize_t j = 0; j < editionLen; j++) {
+				DOMNode *editionNode = editionList->item(j);
+				DOMElement *editionElm = dynamic_cast<DOMElement*>(editionNode);
+
+				if (editionElm->getElementsByTagName(isbn)->getLength() > 0) {
+					cout << "\t\tisbn: " << XMLString::transcode(editionElm->getElementsByTagName(isbn)->item(0)->getTextContent()) << endl;
+				}
+				cout << "\t\tyear: " << XMLString::transcode(editionElm->getElementsByTagName(year)->item(0)->getTextContent()) << endl;
+				cout << "\t\tprice: " << XMLString::transcode(editionElm->getElementsByTagName(price)->item(0)->getTextContent()) << endl;
+			}
+		}
+
+		/*DOMElement* elementRoot = doc->getDocumentElement();
+
+		// Extract floats
+		DOMElement* floatRoot = (DOMElement *) 
+			elementRoot->getElementsByTagName(XMLString::transcode("book"))->item(0);
+		DOMElement* child = floatRoot->getFirstElementChild();
+		do{
+			cout << "2\n";
+			cout << XMLString::transcode(child->getNodeName()) << endl
+			<< XMLString::transcode(child->getTextContent()) << endl;
+			child = child->getNextElementSibling();
+		}while(child != NULL);
+		
+		//create unicode strings to for later use
+        XMLCh *spdName = XMLString::transcode("speed_result");
+        XMLCh *memName = XMLString::transcode("memory_result");
+        XMLCh *sectName = XMLString::transcode("section_list");
+
+        //go through the nodelist finding the top level child nodes and assign them to the right member variables
+        for(XMLSize_t i=0; i<len; i++){
+			cout << "1.1\n";
+
+            DOMNode *curNode = nlist->item(i);
+            DOMElement *curElm;
+			cout << "1.2\n";
+
+            //safely check for element node before casting
+            if(curNode->getNodeType() == DOMNode::ELEMENT_NODE){
+				cout << "1.3\n";
+                curElm = dynamic_cast<DOMElement*>(curNode);
+            }
+
+            //check the tag name and assign accordingly
+            if(XMLString::equals(spdName, curElm->getTagName())){
+                spdResult = curElm;
+            }
+            else if(XMLString::equals(memName, curElm->getTagName())){
+                memResult = curElm;
+            }
+            else if(XMLString::equals(sectName, curElm->getTagName())){
+                sectList = curElm;
+            }
+			cout << curNode->getNodeName() << " " << curNode->getNodeValue() << endl;
+        }
+		cout << "2\n";
+
+        //release the memory occupied by the encoded strings
+        XMLString::release(&spdName);
+        XMLString::release(&memName);
+        XMLString::release(&sectName);*/
 	}
 	catch (const XMLException& toCatch) {
 		char* message = XMLString::transcode(toCatch.getMessage());
